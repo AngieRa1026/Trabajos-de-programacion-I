b45b6c15e29231c8bdd4e18701a51a48a235071b
Angie Ramirez <angieramirez1026@gmail.com>
2025-03-18 21:38:01 -0500

    clase 18 marzo

---
+++ src/Ejercicio15OrdenAlfabetico.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Ejercicio15OrdenAlfabetico {   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un nombre");
+        String nombre1 = scanner.nextLine();
+        System.out.println("Ingrese otro nombre diferente");
+        String nombre2 = scanner.nextLine();
+        scanner.close();
+
+        if (nombre1.compareToIgnoreCase(nombre2) <0) {
+            System.out.println("\nNombres ordenados alfabeticamente:");
+        System.out.println("1. " + nombre1);
+        System.out.println("2. " + nombre2);
+        } else {
+        }
+        
+        System.out.println("\nNombres ordenados alfabéticamente:");
+        System.out.println("1. " + nombre2);
+        System.out.println("2. " + nombre1);
+        scanner.close();
+    } 
+}
